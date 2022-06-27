959b30772139ed88449821ad3407c9985d35a371
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2022-06-27 21:51:43 +0600

    done navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./styles.css";
 import NavItem from "./NavItem";
 
 export default function App() {
-  const items = ["aaa", "bbb", "cccc"];
+  const navs = ["Fast Pack", "Outbound", "Handover"];
   return (
     <div className="h-screen w-screen flex items-center justiry-center">
       <div className="w-[300px] h-[45px] flex items-center justify-between p-1 border rounded-full">
-        <NavItem items={items} />
+        <NavItem navs={navs} />
       </div>
     </div>
   );
--- src/NavItem.js
+++ src/NavItem.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
-export default function NavItem({ items }) {
-  const [activeTitle, setActiveTitle] = useState("aaa");
+export default function NavItem({ navs }) {
+  const [activeNav, setActiveNav] = useState("Fast Pack");
   return (
     <>
-      {items.map((item) => (
+      {navs.map((nav) => (
         <div
-          onClick={() => setActiveTitle(item)}
-          className={`w-fit text-center h-full px-8 py-3 flex items-center cursor-pointer justify-center rounded-full ${
-            activeTitle === item ? "bg-red-500" : null
+          onClick={() => setActiveNav(nav)}
+          className={`w-fit text-center h-full px-8 py-3 flex items-center cursor-pointer justify-center rounded-full whitespace-nowrap ${
+            activeNav === nav ? "bg-red-500" : null
           }`}
-          key={item}
+          key={nav}
         >
-          {item}
+          {nav}
         </div>
       ))}
     </>
