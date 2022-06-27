a6b4c153b5ec4f1429a14f79185360d35a785ec0
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2022-06-27 23:43:44 +0600

    update nav

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   const navs = ["Fast Pack", "Outbound", "Handover"];
   return (
     <div className="h-screen w-screen flex items-center justiry-center">
-      <div className="w-[300px] h-[45px] flex items-center justify-between p-1 border rounded-full">
+      <div className="w-[410px] h-[45px] flex items-center justify-between p-1 border rounded-full">
         <NavItem navs={navs} />
       </div>
     </div>
